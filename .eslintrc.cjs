c5c313ff0a7ed36675135636b37e9f93f6939ca3
Roberto Chavarria <rchavarria77@outlook.com>
2023-09-20 11:17:57 -0600

    initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,25 @@
+module.exports = {
+  env: { browser: true, es2021: true, node: true, jest: true },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
+    "plugin:prettier/recommended",
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: { react: { version: "18.2" } },
+  plugins: ["react-refresh"],
+  rules: {
+    "react-refresh/only-export-components": "warn",
+    "react/prop-types": 0,
+    indent: ["error", 2],
+    "linebreak-style": 1,
+  },
+};
