7354aab9675fcc261c07e61f8481bb33279b7c0d
Roberto Chavarria <rchavarria77@outlook.com>
2023-09-21 12:06:12 -0600

    eslintrc file linted

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,25 +1,25 @@
 module.exports = {
   env: { browser: true, es2021: true, node: true, jest: true },
   extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:react/jsx-runtime",
-    "plugin:react-hooks/recommended",
-    "plugin:prettier/recommended",
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended'
   ],
   parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
+    ecmaVersion: 'latest',
+    sourceType: 'module',
     ecmaFeatures: {
-      jsx: true,
-    },
+      jsx: true
+    }
   },
-  settings: { react: { version: "18.2" } },
-  plugins: ["react-refresh"],
+  settings: { react: { version: '18.2' } },
+  plugins: ['react-refresh'],
   rules: {
-    "react-refresh/only-export-components": "warn",
-    "react/prop-types": 0,
-    indent: ["error", 2],
-    "linebreak-style": 1,
-  },
+    'react-refresh/only-export-components': 'warn',
+    'react/prop-types': 0,
+    indent: ['error', 2],
+    'linebreak-style': 1
+  }
 };
